ac6240f970c59d3a5db3fac60afbdc69f478633b
lei <lei_zhy@163.com>
2020-06-25 20:12:22 +0800

    提交前端代码

---
+++ front/admin-v/src/views/common/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mod-home">
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+      <h2>Ego-Shop项目致力于为中小企业打造一个完整、易于维护的开源的电商系统，采用现阶段流行技术实现。后台管理系统包含商品管理、订单管理、运费模板、规格管理、会员管理、运营管理、内容管理、统计报表、权限管理、设置等模块。</h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.mod-home {
+  line-height: 1.5;
+}
+</style>
+
